c1afb8312e0d852440cc275e3dd5495e4bf86438
jennytsou <50720457+jennytsou@users.noreply.github.com>
2021-02-07 14:56:55 -0700

    Add files via upload

--- stockApp.py
+++ stockApp.py
@@ -12,6 +12,7 @@ import datetime
 import numpy as np
 import pandas as pd
 from pandas_datareader import data, wb
+import html5lib
 
 import requests
 import matplotlib
@@ -83,63 +84,9 @@ def index():
 def stock():
     if request.method == 'POST':
         ticker = request.form['Shares']
-        if not ticker:
-            flash('A share is required!')
-        else:
-            return redirect(url_for('quote', ticker=ticker))
+        return redirect(url_for('post', page=ticker, checks='index'))
     return render_template('shares.html')
 
-
-#---------------------------------------------
-def fetch_price(ticker):
-    data = requests.get('https://financialmodelingprep.com/api/v3/stock/real-time-price/{}'.format(ticker.upper()), params={'apikey':'demo'}).json()
-    if data and "price" in data:
-        return data["price"]
-    else:
-        return "none"
-
-@app.route('/stocks/<ticker>')
-def quote(ticker):
-     stock_price = fetch_price(ticker)
-#     return "This price of {} is {}".format(ticker, stock_price)
-
-
-     symbol =request.args.get('symbol', default=ticker)
-     period = request.args.get('period', default="1y")
-     interval = request.args.get('interval', default="1mo")
-
-	#pull the quote
-     quote = yf.Ticker(symbol)	
-     amazon_weekly= get_data("amzn", start_date="12/04/2009", end_date="12/04/2019", index_as_date = True, interval="1wk")
-
-     return render_template('simple.html', tables=[df.to_html(classes='data')], titles=df.columns.values)
-#     df = amazon_weekly.values.tolist()
-#     JSONP_data = jsonify(df)
-#     return JSONP_data
-#     dow_list = si.tickers_dow()
-
-
-#     quote = yf.Ticker(symbol)
-#     return quote.info
-#--------------------------------------------------
-# API route for pulling the stock history
-#--------------------------------------------------
-
-@app.route("/history")
-def display_history():
-	#get the query string parameters
-	symbol = request.args.get('symbol', default="AAPL")
-	period = request.args.get('period', default="1y")
-	interval = request.args.get('interval', default="1mo")
-
-	#pull the quote
-	quote = yf.Ticker(symbol)	
-	#use the quote to pull the historical data from Yahoo finance
-	hist = quote.history(period=period, interval=interval)
-	#convert the historical data to JSON
-	data = hist.to_json()
-	#return the JSON in the HTTP response
-	return data
 
 #---------------------------------------------
 
@@ -159,26 +106,21 @@ def signin():
         state = request.form['State']
         budget = request.form['Budget']
 
-        if not (fname or lname):
-            flash('Name is required!')
-        elif not budget:
-            flash('Budget is required!')
+        if int(budget) <= 1500:
+            base = 0
+        elif int(budget) > 1500 and int(budget) <= 2500:
+            base = int(budget)*0.1
+        elif int(budget) > 2500 and int(budget) <= 4000:
+            base = int(budget)*0.12
         else:
-            if int(budget) <= 1500:
-                base = 0
-            elif int(budget) > 1500 and int(budget) <= 2500:
-                base = int(budget)*0.1
-            elif int(budget) > 2500 and int(budget) <= 4000:
-                base = int(budget)*0.12
-            else:
-                base = int(budget)*0.15
-            basebudge = base
+            base = int(budget)*0.15
+        basebudge = base
 
-            conn = initial_db_connection()
-            conn.execute('INSERT INTO users (FirstName, LastName, Age, Street, Apartment, Zipcode, State, Budget, Base) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)',
-                        (fname, lname, age, street, apt, zip, state, budget, base))
-            conn.commit()
-            return redirect(url_for('advice'))
+        conn = initial_db_connection()
+        conn.execute('INSERT INTO users (FirstName, LastName, Age, Street, Apartment, Zipcode, State, Budget, Base) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                    (fname, lname, age, street, apt, zip, state, budget, base))
+        conn.commit()
+        return redirect(url_for('advice'))
 
     return render_template('users.html', post=post)
 
@@ -252,7 +194,7 @@ def dividend():
 @app.route('/signIn/advice/dividend/display', methods=('GET', 'POST'))
 def display():
 
-#    divid = request.args.get('divid')
+    divid = request.args.get('divid')
     conn =get_db_connection()
     posts = conn.execute('SELECT * FROM dividends').fetchall()
     conn.close()
@@ -264,15 +206,21 @@ def display():
 
 #------------------------------------------------
 
-@app.route('/<page>', methods=('GET', 'POST'))
-def post(page):
+@app.route('/<page>/<checks>', methods=('GET', 'POST'))
+def post(page, checks):
+
 
     hsitorical_datas = {}
     labels = []
     values = []
-    start = "01/01/2021"
-    end = datetime.date.today()
-    interval="1d"
+
+    if request.method == 'POST':
+        start = request.form['Starts']
+        end = request.form['Ends']
+    else:
+        start = "01/01/2021"
+        end = datetime.date.today()
+
     quote = yf.Ticker(page)
     quotename = quote.info["shortName"]
     legend = quotename + " Historical Stock Line Chart"
@@ -286,23 +234,9 @@ def post(page):
     for i in t_values:
         values.append(round(i,2))
 
-    while True:
-        if request.method == 'POST':
-            start = request.form['Starts']
-            end = request.form['Ends']
+    return render_template('chart.html', values=values, labels=labels, legend=legend, checks=checks)
 
-            historical_datas = si.get_data(page, start_date=start, end_date=end)
-            t_labels = historical_datas.index
-            labels = []
-            values = []
+#------------------------------------------------
 
-            for i in t_labels:
-                labels.append(i.date())
-            t_values = historical_datas.adjclose
 
-            for i in t_values:
-                values.append(round(i,2))
 
-#            return redirect(url_for('display'))
-
-        return render_template('chart.html', values=values, labels=labels, legend=legend, start=start, end=end)
